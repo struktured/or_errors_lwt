903ae4acf56cb03089146b61e17b8b87c7567585
carm <carmelo.piccione@gmail.com>
2015-12-11 06:22:44 -0500

    join, both, implemented

--- src/or_error.ml
+++ src/or_error.ml
@@ -3,37 +3,31 @@ open Or_errors.Std
 
 module Make(Result:RESULT) = struct
   include Lwt
-  
+
   module Error = Lwt_error
   module Result = Result
   module Monad_infix =
   struct
-    include Lwt.Infix 
+    include Lwt.Infix
     let (>>|) = (>|=)
   end
   include Monad_infix
-  let both = failwith("nyi")
   let ignore t = return @@ Lwt.ignore_result t
   let all = nchoose
   let all_ignore = Lwt.join
-  let join = failwith("nyi")
+  let join t = Lwt.bind t (fun t' -> Lwt.bind t'
+      (fun ok -> Lwt.return ok))
+  let both a b = join @@ Lwt.map (fun a -> Lwt.map (fun b -> (a,b)) b) a
   let map t ~f = map f t
 end
 
-module type S = 
+module type S =
 sig
  include module type of Lwt
  include OR_ERROR with type 'a t := 'a t
 end
 
-(**
-module type S = 
-sig
-module Result : RESULT
-include module type of Make(Result) with module Result := Result
-end **)
-
-module Signature(Result:RESULT) : OR_ERROR = 
+module Signature(Result:RESULT) : OR_ERROR =
 struct
   include Make(Result)
 end
